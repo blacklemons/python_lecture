2765fc8bb2e752be874c334d915611028095f04d
Your Name <you@example.com>
2021-05-24 17:36:07 +0900

    Second Commit

---
+++ circle_with_var.py
@@ -0,0 +1,13 @@
+import math
+
+radius = 4.0
+
+print('원의 반지름 :', radius)
+print('원의 면적 : ', math.pi * radius * radius)
+print('원의 면적 : ', math.pi * radius ** 2)
+print('원의 둘레', 2.0 * math.pi * radius )
+
+A = 'a'
+
+print('A : ',A)
+print('A : ',chr(ord(A)+1))
